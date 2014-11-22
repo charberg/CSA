8e933b9524afbb14c0709f07d88b0e8a2577ecbf
chrisB25 <cbriglio@live.ca>
2014-11-22 00:15:59 -0500

    added some arrays

--- php/classes/OnScheduleCourseCalculator.php
+++ php/classes/OnScheduleCourseCalculator.php
@@ -151,96 +151,97 @@
 		function calculateConflictFreeSchedules() {
 			
 			//break apart courses into lists per course
-			$class1 = new SectionList();
-			$class1->addItem($this->courses[0]);
+			$classlist1 = new SectionList();
+			$classlist1->addItem($this->courses[0]);
 			unset($this->courses[0]);
 			
 			for ($i = 0; $i < count($this->courses);$i = $i + 1) {
 				
-				if ($this->courses[$i]->subjectID == $class1[0]->subjectID
-					&& $this->courses[$i]->courseNum == $class1[0]->courseNum) {
+				if ($this->courses[$i]->subjectID == $classlist1[0]->subjectID
+					&& $this->courses[$i]->courseNum == $classlist1[0]->courseNum) {
 					
-					$class1->addItem($this->courses[$i]);
+					$classlist1->addItem($this->courses[$i]);
 					unset($this->courses[$i]);
 					$i = $i - 1;
 				}
 				
 			}
 			
-			$class2 = new SectionList();
-			$class2->addItem($this->courses[0]);
+			$classlist2 = new SectionList();
+			$classlist2->addItem($this->courses[0]);
 			unset($this->courses[0]);
 			
 			for ($i = 0; $i < count($this->courses);$i = $i + 1) {
 			
-				if ($this->courses[$i]->subjectID == $class2[0]->subjectID
-					&& $this->courses[$i]->courseNum == $class2[0]->courseNum) {
+				if ($this->courses[$i]->subjectID == $classlist2[0]->subjectID
+					&& $this->courses[$i]->courseNum == $classlist2[0]->courseNum) {
 					
-					$class2->addItem($this->courses[$i]);
+					$classlist2->addItem($this->courses[$i]);
 					unset($this->courses[$i]);
 					$i = $i - 1;
 				}
 			
 			}
 			
-			$class3 = new SectionList();
-			$class3->addItem($this->courses[0]);
+			$classlist3 = new SectionList();
+			$classlist3->addItem($this->courses[0]);
 			unset($this->courses[0]);
 			
 			for ($i = 0; $i < count($this->courses);$i = $i + 1) {
 			
-				if ($this->courses[$i]->subjectID == $class3[0]->subjectID
-					&& $this->courses[$i]->courseNum == $class3[0]->courseNum) {
+				if ($this->courses[$i]->subjectID == $classlist3[0]->subjectID
+					&& $this->courses[$i]->courseNum == $classlist3[0]->courseNum) {
 					
-					$class3->addItem($this->courses[$i]);
+					$classlist3->addItem($this->courses[$i]);
 					unset($this->courses[$i]);
 					$i = $i - 1;
 				}
 			
 			}
 			
-			$class4 = new SectionList();
-			$class4->addItem($this->courses[0]);
+			$classlist4 = new SectionList();
+			$classlist4->addItem($this->courses[0]);
 			unset($this->courses[0]);
 			
 			for ($i = 0; $i < count($this->courses);$i = $i + 1) {
 			
-				if ($this->courses[$i]->subjectID == $class4[0]->subjectID
-					&& $this->courses[$i]->courseNum == $class4[0]->courseNum) {
+				if ($this->courses[$i]->subjectID == $classlist4[0]->subjectID
+					&& $this->courses[$i]->courseNum == $classlist4[0]->courseNum) {
 					
-					$class4->addItem($this->courses[$i]);
+					$classlist4->addItem($this->courses[$i]);
 					unset($this->courses[$i]);
 					$i = $i - 1;
 				}
 			
 			}
 			
-			$class5 = new SectionList();
-			$class5->addItem($this->courses[0]);
+			$classlist5 = new SectionList();
+			$classlist5->addItem($this->courses[0]);
 			unset($this->courses[0]);
 			
 			for ($i = 0; $i < count($this->courses);$i = $i + 1) {
 			
-				if ($this->courses[$i]->subjectID == $class5[0]->subjectID
-					&& $this->courses[$i]->courseNum == $class5[0]->courseNum) {
+				if ($this->courses[$i]->subjectID == $classlist5[0]->subjectID
+					&& $this->courses[$i]->courseNum == $classlist5[0]->courseNum) {
 					
-					$class5->addItem($this->courses[$i]);
+					$classlist5->addItem($this->courses[$i]);
 					unset($this->courses[$i]);
 					$i = $i - 1;
 				}
 			
 			}
+			
 			if (count($this->courses) > 0) {	///May not have 6th course, only create final list if 6th course exists
-				$class6 = new SectionList();
-				$class6->addItem($this->courses[0]);
+				$classlist6 = new SectionList();
+				$classlist6->addItem($this->courses[0]);
 				unset($this->courses[0]);
 				
 				for ($i = 0; $i < count($this->courses);$i = $i + 1) {
 				
-					if ($this->courses[$i]->subjectID == $class6[0]->subjectID
-						&& $this->courses[$i]->courseNum == $class6[0]->courseNum) {
+					if ($this->courses[$i]->subjectID == $classlist6[0]->subjectID
+						&& $this->courses[$i]->courseNum == $classlist6[0]->courseNum) {
 						
-						$class6->addItem($this->courses[$i]);
+						$classlist6->addItem($this->courses[$i]);
 						unset($this->courses[$i]);
 						$i = $i - 1;
 					}
@@ -248,8 +249,179 @@
 				}
 			}
 			
-			//Now each posibility of 
-		
+			//Now each posibility of every course to be taken is in their own lists
+			//Now go through each list, pick one and generate schedule
+			//Run through all possibilities
+			
+			//Create arrays to keep track of what times are booked
+			$monday = array("8:00-8:30" => 0,
+							"8:30-9:00" => 0,
+							"9:00-9:30" => 0,
+							"9:30-10:00" => 0,
+							"10:00-10:30" => 0,
+							"10:30-11:00" => 0,
+							"11:00-11:30" => 0,
+							"11:30-12:00" => 0,
+							"12:00-12:30" => 0,
+							"12:30-13:00" => 0,
+							"13:00-13:30" => 0,
+							"13:30-14:00" => 0,
+							"14:00-14:30" => 0,
+							"14:30-15:00" => 0,
+							"15:00-15:30" => 0,
+							"15:30-16:00" => 0,
+							"16:00-16:30" => 0,
+							"16:30-17:00" => 0,
+							"17:00-17:30" => 0,
+							"17:30-18:00" => 0,
+							"18:00-18:30" => 0,
+							"18:30-19:00" => 0,
+							"19:00-19:30" => 0,
+							"19:30-20:00" => 0,
+							"20:00-20:30" => 0,
+							"20:30-21:00" => 0,
+							"21:00-21:30" => 0,
+							"21:30-22:00" => 0,
+							"22:00-22:30" => 0,
+							"22:30-23:00" => 0,
+							"23:00-23:30" => 0,
+							"23:30-24:00" => 0);
+			
+			$tuesday = array("8:00-8:30" => 0,
+							"8:30-9:00" => 0,
+							"9:00-9:30" => 0,
+							"9:30-10:00" => 0,
+							"10:00-10:30" => 0,
+							"10:30-11:00" => 0,
+							"11:00-11:30" => 0,
+							"11:30-12:00" => 0,
+							"12:00-12:30" => 0,
+							"12:30-13:00" => 0,
+							"13:00-13:30" => 0,
+							"13:30-14:00" => 0,
+							"14:00-14:30" => 0,
+							"14:30-15:00" => 0,
+							"15:00-15:30" => 0,
+							"15:30-16:00" => 0,
+							"16:00-16:30" => 0,
+							"16:30-17:00" => 0,
+							"17:00-17:30" => 0,
+							"17:30-18:00" => 0,
+							"18:00-18:30" => 0,
+							"18:30-19:00" => 0,
+							"19:00-19:30" => 0,
+							"19:30-20:00" => 0,
+							"20:00-20:30" => 0,
+							"20:30-21:00" => 0,
+							"21:00-21:30" => 0,
+							"21:30-22:00" => 0,
+							"22:00-22:30" => 0,
+							"22:30-23:00" => 0,
+							"23:00-23:30" => 0,
+							"23:30-24:00" => 0);
+			
+			$wednesday = array("8:00-8:30" => 0,
+							"8:30-9:00" => 0,
+							"9:00-9:30" => 0,
+							"9:30-10:00" => 0,
+							"10:00-10:30" => 0,
+							"10:30-11:00" => 0,
+							"11:00-11:30" => 0,
+							"11:30-12:00" => 0,
+							"12:00-12:30" => 0,
+							"12:30-13:00" => 0,
+							"13:00-13:30" => 0,
+							"13:30-14:00" => 0,
+							"14:00-14:30" => 0,
+							"14:30-15:00" => 0,
+							"15:00-15:30" => 0,
+							"15:30-16:00" => 0,
+							"16:00-16:30" => 0,
+							"16:30-17:00" => 0,
+							"17:00-17:30" => 0,
+							"17:30-18:00" => 0,
+							"18:00-18:30" => 0,
+							"18:30-19:00" => 0,
+							"19:00-19:30" => 0,
+							"19:30-20:00" => 0,
+							"20:00-20:30" => 0,
+							"20:30-21:00" => 0,
+							"21:00-21:30" => 0,
+							"21:30-22:00" => 0,
+							"22:00-22:30" => 0,
+							"22:30-23:00" => 0,
+							"23:00-23:30" => 0,
+							"23:30-24:00" => 0);
+							
+			$thursday = array("8:00-8:30" => 0,
+							"8:30-9:00" => 0,
+							"9:00-9:30" => 0,
+							"9:30-10:00" => 0,
+							"10:00-10:30" => 0,
+							"10:30-11:00" => 0,
+							"11:00-11:30" => 0,
+							"11:30-12:00" => 0,
+							"12:00-12:30" => 0,
+							"12:30-13:00" => 0,
+							"13:00-13:30" => 0,
+							"13:30-14:00" => 0,
+							"14:00-14:30" => 0,
+							"14:30-15:00" => 0,
+							"15:00-15:30" => 0,
+							"15:30-16:00" => 0,
+							"16:00-16:30" => 0,
+							"16:30-17:00" => 0,
+							"17:00-17:30" => 0,
+							"17:30-18:00" => 0,
+							"18:00-18:30" => 0,
+							"18:30-19:00" => 0,
+							"19:00-19:30" => 0,
+							"19:30-20:00" => 0,
+							"20:00-20:30" => 0,
+							"20:30-21:00" => 0,
+							"21:00-21:30" => 0,
+							"21:30-22:00" => 0,
+							"22:00-22:30" => 0,
+							"22:30-23:00" => 0,
+							"23:00-23:30" => 0,
+							"23:30-24:00" => 0);
+							
+			$friday = array("8:00-8:30" => 0,
+							"8:30-9:00" => 0,
+							"9:00-9:30" => 0,
+							"9:30-10:00" => 0,
+							"10:00-10:30" => 0,
+							"10:30-11:00" => 0,
+							"11:00-11:30" => 0,
+							"11:30-12:00" => 0,
+							"12:00-12:30" => 0,
+							"12:30-13:00" => 0,
+							"13:00-13:30" => 0,
+							"13:30-14:00" => 0,
+							"14:00-14:30" => 0,
+							"14:30-15:00" => 0,
+							"15:00-15:30" => 0,
+							"15:30-16:00" => 0,
+							"16:00-16:30" => 0,
+							"16:30-17:00" => 0,
+							"17:00-17:30" => 0,
+							"17:30-18:00" => 0,
+							"18:00-18:30" => 0,
+							"18:30-19:00" => 0,
+							"19:00-19:30" => 0,
+							"19:30-20:00" => 0,
+							"20:00-20:30" => 0,
+							"20:30-21:00" => 0,
+							"21:00-21:30" => 0,
+							"21:30-22:00" => 0,
+							"22:00-22:30" => 0,
+							"22:30-23:00" => 0,
+							"23:00-23:30" => 0,
+							"23:30-24:00" => 0);
+							
+			
+			$class1 = $classlist1[0];
+			
 		}
 		
 		
