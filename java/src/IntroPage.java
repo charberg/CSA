49252cd4a1cfed2e43f1438bcb6699a2f925051f
David Briglio <david.briglio33@gmail.com>
2014-11-21 20:06:03 -0500

    Updated IntroPage to show label.

--- java/src/IntroPage.java
+++ java/src/IntroPage.java
@@ -1,10 +1,27 @@
+import java.awt.Dimension;
+import java.awt.Font;
+
 import javax.swing.*;
 
 
 public class IntroPage extends JFrame{
 	
+	private JPanel panel;
+	private JLabel welcome, infoTxt;
+	
 	public IntroPage(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1200,800);
+		panel = new JPanel();
 		
+		welcome = new JLabel("Welcome to the Briglio course selector!");
+		//welcome.setPreferredSize(new Dimension(5,5));
+		welcome.setFont(new Font("Calibri",1,40));
+		panel.add(welcome);
+		
+		
+		add(panel);
+		setVisible(true);
 	}
 	
 	
